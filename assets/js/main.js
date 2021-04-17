f6568190fab516f35ecfb3a67fab01b35cb85a45
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2021-04-17 18:08:38 +0200

    About us

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+let xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4) {
+    document.querySelector(".navbar-include").innerHTML = xhr.responseText;
+  }
+};
+xhr.open("GET", "/templates/navbar.html");
+xhr.send();
+
+let xhr_footer = new XMLHttpRequest();
+xhr_footer.onreadystatechange = function () {
+  if (xhr.readyState === 4) {
+    document.querySelector(".footer-include").innerHTML =
+      xhr_footer.responseText;
+  }
+};
+xhr_footer.open("GET", "/templates/footer.html");
+xhr_footer.send();
