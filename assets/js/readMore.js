8d99ce602062a0e11dc5b783113f713456c48728
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2021-04-29 23:42:46 +0200

    Single car - basics

---
+++ assets/js/readMore.js
@@ -0,0 +1,12 @@
+function myFunction() {
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (moreText.style.display === "inline") {
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
