f916520ae39af3a07ff8879564e429384670da7b
XcombeX <obada97almonajed@gmail.com>
2021-05-04 02:29:25 +0200

    read more/less animation

--- assets/js/readMore.js
+++ assets/js/readMore.js
@@ -1,4 +1,4 @@
-function myFunction() {
+/* function myFunction() {
   var moreText = document.getElementById("more");
   var btnText = document.getElementById("myBtn");
 
@@ -9,4 +9,38 @@ function myFunction() {
     btnText.innerHTML = "Read less";
     moreText.style.display = "inline";
   }
+}
+ */
+let box = document.getElementById('box');
+let isActive = false;
+console.log( box.scrollHeight );
+box.setAttribute( 'data', { 'height': box.offsetHeight} );
+
+box.style.height = 0;
+document.getElementById("toggle").addEventListener('click', function() {
+  slideToggle();
+});
+
+function slideToggle() {
+
+  var moreText = document.getElementById("box");
+  var btnText = document.getElementById("toggle");
+    	isActive = !isActive;
+
+			if ( isActive ) {
+
+				document.documentElement.classList.add( 'dropdown--is-open');
+        box.style.height = box.dataset.height;
+        btnText.innerHTML = "Read less";
+
+        
+			} else {
+
+				document.documentElement.classList.remove( 'dropdown--is-open');
+        box.style.height = 0;
+        btnText.innerHTML = "Read more";
+
+
+			}
+  
 }
